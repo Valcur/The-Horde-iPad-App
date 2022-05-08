da56cd208e0c10227ab257a052122c0275af93cd
Loic D <loic.danj@gmail.com>
2022-05-08 15:52:13 +0200

    Initial Commit

---
+++ Horde/HordeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HordeApp.swift
+//  Horde
+//
+//  Created by Loic D on 08/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct HordeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
